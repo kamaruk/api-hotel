aeacd4ccf3e86434d5b9aeea57a20cb5ea96f553
vovas <vova.sazan2017@yandex.ru>
2025-06-09 11:28:22 +0900

    add

--- hotel_api/hotel_api/settings.py
+++ hotel_api/hotel_api/settings.py
@@ -137,3 +137,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['91.107.125.169', 'api-hotel.fvds.ru']
